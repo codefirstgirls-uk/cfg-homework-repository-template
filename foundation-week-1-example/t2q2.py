3f8ca724e17f1069237a5094200a631a7854d752
hakan <erik.hakansson96@gmail.com>
2022-01-27 22:07:26 +0100

    minor typo in imports: also used module syntax rather than function import in acc. /w styleguide

--- foundation-week-1-example/t2q2.py
+++ foundation-week-1-example/t2q2.py
@@ -1,6 +1,6 @@
 """Task2, Question 2"""
 import unittest
-from t2q1 import add
+import t2q1
 # Notice how we can import and reuse code defined in other questions using imports;
 # note that if the import does not work, it is likely that you are missing an __init__.py file in the homework directory. 
 
@@ -8,13 +8,13 @@ class AddTests(unittest.TestCase):
     """This is a test to ensure that the code defined in question 2 is behaving correctly"""
     
     def test_number_input(self):
-        self.assertEqual(add(2,2), 4)
+        self.assertEqual(t2q1.add(2,2), 4)
 
     def test_string_input(self):
-        self.assertEqual(add("hello, ", "world!"), "hello, world!")
+        self.assertEqual(t2q1.add("hello, ", "world!"), "hello, world!")
 
     def test_mixed_input(self):
-        self.assertRaises(TypeError, add, "1", 2)
+        self.assertRaises(TypeError, t2q1.add, "1", 2)
 
 
 # this magic line runs all the test if you run this file
